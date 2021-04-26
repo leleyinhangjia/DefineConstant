1c85cdaf8492c30e83009d49c237437a57eec85a
ZhengLeyin <zhenghan1221@163.com>
2021-04-26 16:06:42 +0800

    fix:提交相关宏定义

--- DefineConstant.h
+++ DefineConstant.h
@@ -115,6 +115,129 @@ blue:((float)(rgbValue & 0xFF))/255.0 alpha:1.0]
 #define IOS8_OR_LATER  ([[[UIDevice currentDevice] systemVersion] floatValue] >= 8.0)
 #define IOS7_OR_LATER  ([[[UIDevice currentDevice] systemVersion] floatValue] >= 7.0)
 
+// 判断 iPad
+#define DX_UI_IS_IPAD (UI_USER_INTERFACE_IDIOM() == UIUserInterfaceIdiomPad)
+
+// 判断iPhone X
+#define DX_Is_iPhoneX ([UIScreen instancesRespondToSelector:@selector(currentMode)] ? CGSizeEqualToSize(CGSizeMake(1125, 2436), [[UIScreen mainScreen] currentMode].size) : NO)
+
+//判断iPHoneXr | 11
+#define DX_Is_iPhoneXR ([UIScreen instancesRespondToSelector:@selector(currentMode)] ? CGSizeEqualToSize(CGSizeMake(828, 1792), [[UIScreen mainScreen] currentMode].size) && !DX_UI_IS_IPAD : NO)
+
+//判断iPHoneXs | 11Pro
+#define DX_Is_iPhoneXS ([UIScreen instancesRespondToSelector:@selector(currentMode)] ? CGSizeEqualToSize(CGSizeMake(1125, 2436), [[UIScreen mainScreen] currentMode].size) && !DX_UI_IS_IPAD : NO)
+
+//判断iPhoneXs Max | 11ProMax
+#define DX_Is_iPhoneXS_MAX ([UIScreen instancesRespondToSelector:@selector(currentMode)] ? CGSizeEqualToSize(CGSizeMake(1242, 2688), [[UIScreen mainScreen] currentMode].size) && !DX_UI_IS_IPAD : NO)
+
+//判断iPhone12_Mini
+#define DX_Is_iPhone12_Mini ([UIScreen instancesRespondToSelector:@selector(currentMode)] ? CGSizeEqualToSize(CGSizeMake(1080, 2340), [[UIScreen mainScreen] currentMode].size) && !DX_UI_IS_IPAD : NO)
+
+//判断iPhone12 | 12Pro
+#define DX_Is_iPhone12 ([UIScreen instancesRespondToSelector:@selector(currentMode)] ? CGSizeEqualToSize(CGSizeMake(1170, 2532), [[UIScreen mainScreen] currentMode].size) && !DX_UI_IS_IPAD : NO)
+
+//判断iPhone12 Pro Max
+#define DX_Is_iPhone12_ProMax ([UIScreen instancesRespondToSelector:@selector(currentMode)] ? CGSizeEqualToSize(CGSizeMake(1284, 2778), [[UIScreen mainScreen] currentMode].size) && !DX_UI_IS_IPAD : NO)
+
+//x系列
+#define IS_iPhoneXStyle (DX_Is_iPhoneX || DX_Is_iPhoneXR || DX_Is_iPhoneXS || DX_Is_iPhoneXS_MAX || DX_Is_iPhone12_Mini || DX_Is_iPhone12 || DX_Is_iPhone12_ProMax)
+
+//状态栏的高度
+#define HitoStatusBarHeight ([[UIApplication sharedApplication] statusBarFrame].size.height)
+//导航栏的高度
+#define HitoNavBarHeight ((DX_Is_iPhoneXR) ? 48.0:((DX_Is_iPhone12 || DX_Is_iPhone12_ProMax) ? 47.0:44.0))
+
+//iphoneX-SafeArea的高度
+#define HitoSafeAreaHeight ([[UIApplication sharedApplication] statusBarFrame].size.height>20?34:0)
+//分栏+iphoneX-SafeArea的高度
+#define HitoTabBarHeight (49+HitoSafeAreaHeight)
+//导航栏+状态栏的高度
+#define HitoTopHeight (HitoStatusBarHeight + HitoNavBarHeight)
+
+#define kScale(P)   ((P) * ([UIScreen mainScreen].bounds.size.width / 375.f))
+
+#define NaviH HitoTopHeight // 812是iPhoneX的高度
+
+#define TabbarH (IS_iPhoneXStyle ? 83 : 49)
+
+#define effectViewH  (IS_iPhoneXStyle ? 34 : 0)
+
+#define TopeffectViewH  (IS_iPhoneXStyle ? 44 : 0)
+
+#define FontSize(size) [UIFont systemFontOfSize:size]
+
+#define kSaveMyDefault(A,B) [[NSUserDefaults standardUserDefaults] setObject:B forKey:A]
+
+#define kFetchMyDefault(A) [[NSUserDefaults standardUserDefaults] objectForKey:A]
+
+/**所有颜色配置*/
+#define kThemeColor   UIColorRGB(0x0098ed) //主题色单独定制 防止全局替换出现特定颜色有冲突
+
+#define k0x0098edColor   UIColorRGB(0x0098ed)
+#define k0xffffffColor   UIColorRGB(0xffffff)
+#define k0x000000Color   UIColorRGB(0x000000)
+#define k0xccccccColor   UIColorRGB(0xcccccc)
+#define k0xddddddColor   UIColorRGB(0xdddddd)
+#define k0x666666Color   UIColorRGB(0x666666)
+#define k0x999999Color   UIColorRGB(0x999999)
+#define k0xe5e5e5Color   UIColorRGB(0xe5e5e5)
+#define k0xeaeaeaColor   UIColorRGB(0xe5e5e5)
+
+
+#define k0xf4f4f4Color   UIColorRGB(0xf4f4f4)
+#define k0xf8f8f8Color   UIColorRGB(0xf8f8f8)
+#define k0xf8f8f8Color   UIColorRGB(0xf8f8f8)
+#define k0xff8400Color   UIColorRGB(0xff8400)
+#define k0xff6969Color   UIColorRGB(0xff6969)
+#define k0xf7931eColor   UIColorRGB(0xf7931e)
+#define k0xff9000Color   UIColorRGB(0xff9000)
+#define k0x18c884Color   UIColorRGB(0x18c884)
+#define k0xff9b48Color   UIColorRGB(0xff9b48)
+#define k0xff6633Color   UIColorRGB(0xff6633)
+
+/**所有字体配置*/
+///////////文字
+//#define kTextColorLight
+#define kTextColorNormal    [UIColor colorWithRed:136.0f/255.0f green:136.0f/255.0f blue:136.0f/255.0f alpha:1.0f]
+#define kTextColorDark      [UIColor colorWithRed:51.0f/255.0f green:51.0f/255.0f blue:51.0f/255.0f alpha:1.0f]//0X333333
+#define kTextColorImport
+#define kTextColorReverse
+#define kTextColorLink
+#define kTextColorTitle
+#define kTextSizeBig         18.0*KsuitParam   //大字体，标题等
+#define kTextSizeOfRarBig    17.0*KsuitParam   //大字体，标题等
+#define kTextSizeBigLess     16.0*KsuitParam
+#define kTextSizeMid         15.0*KsuitParam   //中等字体，列表内容等
+#define kTextSizeMidLess     14.0*KsuitParam
+#define kTextSizeOfRarLess   13.0*KsuitParam  //小标题存在13.0
+#define kTextSizeSml         12.0*KsuitParam  //小字体，属性等
+#define kTextOfCellSizeSml   11.0*KsuitParam   //cell.中小字体属性,比如(意见反馈列表,反馈时间字体)
+#define kTextSizeSmlLess     10.0*KsuitParam
+#define kTextSizeLess        9.0*KsuitParam
+#define kTextSizeTitle
+
+///////////字体
+// swift中字体也需要使用字体名称，在UIConstants.swift文件中使用
+#define sFontNameRegular    @"PingFangSC-Regular"  // 常规体字体名称
+#define sFontNameMedium     @"PingFangSC-Medium"   // 中等字体名称
+#define sFontNameBold       @"PingFangSC-Semibold" // 粗体字体名称
+#define FontRegular(fontSize)    [UIFont fontWithName:sFontNameRegular size:fontSize]  // 常规体
+#define FontMedium(fontSize)     [UIFont fontWithName:sFontNameMedium size:fontSize]   // 中等
+#define FontBold(fontSize)       [UIFont fontWithName:sFontNameBold size:fontSize]     // 粗体
+#define FontDINRegular(fontSize) [UIFont fontWithName:@"DIN-Regular" size:fontSize]    // DIN常规体
+#define FontDINMedium(fontSize)  [UIFont fontWithName:@"DIN-Medium" size:fontSize]     // DIN中等
+#define FontDINBold(fontSize)    [UIFont fontWithName:@"DIN-Bold" size:fontSize]       // DIN粗体
+
+/**主分段标题设置  举例:考试界面(未交卷,已交卷) 字体统一 15,底部线条 16宽,2高 类似界面全部统一*/
+#define lineBottomViewWidth   16
+#define lineBottomViewHeight  2
+
+//iOS 14开始，全面屏iPhone的状态栏高度不一定是44了，比如下面就是这些设备在iOS 14.1上的状态栏高度。
+//设备状态栏高度
+//iPhone XR/11     48
+//iPhone X/11 Pro/ 11 Pro Max/12 mini     44
+//iPhone 12/12 Pro/Pro Max     47
+
 /** 打印 NSlog */
 
 #ifdef DEBUG
